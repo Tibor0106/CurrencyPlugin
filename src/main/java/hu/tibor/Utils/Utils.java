36d21f007fc8eaa505eb37ddc5a31b2af5733ec3
parag <paragh.tibor71@gmail.com>
2023-09-26 17:40:59 +0200

    Plugin

--- src/main/java/hu/tibor/Utils/Utils.java
+++ src/main/java/hu/tibor/Utils/Utils.java
@@ -1,4 +1,13 @@
 package hu.tibor.Utils;
 
+import org.bukkit.plugin.Plugin;
+
 public class Utils {
+
+    private  Plugin plugin;
+
+    //Class's constructor
+    public Utils(Plugin pl){
+        this.plugin = pl;
+    }
 }
