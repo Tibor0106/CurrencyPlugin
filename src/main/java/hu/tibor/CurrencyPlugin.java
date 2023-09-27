337e086f8017ea137cf9eed78081dc98fbed37c8
parag <paragh.tibor71@gmail.com>
2023-09-27 16:43:31 +0200

    Add Mysql

--- src/main/java/hu/tibor/CurrencyPlugin.java
+++ src/main/java/hu/tibor/CurrencyPlugin.java
@@ -1,13 +1,15 @@
 package hu.tibor;
 
 import hu.tibor.Commands.CCurrency_player;
+import hu.tibor.MySql.MySql;
 import hu.tibor.Utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CurrencyPlugin extends JavaPlugin {
     private Utils utils;
-    private static Currency currency;
+    public MySql mySql;
+
 
     @Override
     public void onEnable() {
@@ -15,17 +17,18 @@ public final class CurrencyPlugin extends JavaPlugin {
                           //Startup message
         Bukkit.getLogger().info(this.getName()+"§a Ready to use!");
 
+
         //
         utils = new Utils(this);
-        currency = new Currency(this);
-
+        mySql = new MySql(this, true);
         //commands
         getServer().getPluginCommand("currency-player").setExecutor(new CCurrency_player());
     }
     @Override
     public void onDisable() {
     }
-    public static Currency getCurrency(){
-        return  currency;
+    public MySql getMysqlDataHandler(){
+        return mySql;
     }
+
 }
